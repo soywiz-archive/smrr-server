f79caed6dbf16e733ee09dd7e65c273592847501
soywiz <soywiz@gmail.com>
2011-10-06 15:34:39 +0200

    - Fixed #2 : Allow specify parameters when starting the server - Updated version

--- server-cs/SimpleMassiveRealtimeRankingServer/Program.cs
+++ server-cs/SimpleMassiveRealtimeRankingServer/Program.cs
@@ -11,6 +11,7 @@ using CSharpUtils.Extensions;
 using System.IO;
 using CSharpUtils.Streams;
 using SimpleMassiveRealtimeRankingServer.Server;
+using CSharpUtils.Getopt;
 
 namespace SimpleMassiveRealtimeRankingServer
 {
@@ -18,9 +19,47 @@ namespace SimpleMassiveRealtimeRankingServer
 	{
 		static void Main(string[] args)
 		{
-			var ServerHandler = new ServerHandler("127.0.0.1", 9777);
-			ServerHandler.ListenStart();
-			ServerHandler.AcceptClientLoop();
+			try
+			{
+				string BindIp = "127.0.0.1";
+				int BindPort = 9777;
+
+				var Getopt = new Getopt(args);
+				Getopt.AddRule("-v", () =>
+				{
+					Console.WriteLine(new ServerManager().Version);
+					Environment.Exit(0);
+				});
+				Getopt.AddRule("-i", (string Value) => { BindIp = Value; });
+				Getopt.AddRule("-p", (int Value) => { BindPort = Value; });
+				Getopt.AddRule(new string[] { "-h", "-?", "--help" }, () =>
+				{
+					Console.WriteLine("Simple Massive Realtime Ranking Server - {0} - Carlos Ballesteros Velasco - 2011-2011", new ServerManager().Version);
+					Console.WriteLine("Lastest version: https://github.com/soywiz/smrr-server");
+					Console.WriteLine("");
+					Console.WriteLine("Parameters:");
+					Console.WriteLine("    -i    Sets the binding ip. Example: -i=192.168.1.1");
+					Console.WriteLine("    -p    Sets the binding port. Example: -p=7777");
+					Console.WriteLine("");
+					Console.WriteLine("    -v    Shows the version of the server");
+					Console.WriteLine("    -h    Shows this help");
+					Console.WriteLine("");
+					Console.WriteLine("Examples:");
+					Console.WriteLine("    SimpleMassiveRealtimeRankingServer -i=0.0.0.0 -p=7777");
+					Environment.Exit(-1);
+				});
+				Getopt.Process();
+
+				var ServerHandler = new ServerHandler(BindIp, BindPort);
+				ServerHandler.ListenStart();
+				ServerHandler.AcceptClientLoop();
+				Environment.Exit(0);
+			}
+			catch (Exception Exception)
+			{
+				Console.WriteLine(Exception);
+				Environment.Exit(-1);
+			}
 		}
 	}
 }
--- server-cs/SimpleMassiveRealtimeRankingServer/Server/ServerManager.cs
+++ server-cs/SimpleMassiveRealtimeRankingServer/Server/ServerManager.cs
@@ -22,6 +22,29 @@ namespace SimpleMassiveRealtimeRankingServer.Server
 			public byte MinorVersion;
 			public byte RevisionVersion;
 			public byte PatchVersion;
+
+			public override string ToString()
+			{
+				var Ret = String.Format(
+					"{0}.{1}.{2}",
+					MajorVersion, MinorVersion, RevisionVersion
+				);
+
+				switch (PatchVersion)
+				{
+					case 1: Ret += "-alpha"; break;
+					case 2: Ret += "-beta"; break;
+					case 11: Ret += "-RC1"; break;
+					case 12: Ret += "-RC2"; break;
+					case 13: Ret += "-RC3"; break;
+					case 14: Ret += "-RC4"; break;
+					case 15: Ret += "-RC5"; break;
+					case 16: Ret += "-RC6"; break;
+					case 17: Ret += "-RC7"; break;
+				}
+
+				return Ret;
+			}
 		}
 
 		public ServerIndices ServerIndices = new ServerIndices();
@@ -32,10 +55,10 @@ namespace SimpleMassiveRealtimeRankingServer.Server
 			{
 				return new VersionStruct()
 				{
-					MajorVersion = 1,
-					MinorVersion = 1,
-					RevisionVersion = 0,
-					PatchVersion = 0,
+					MajorVersion = 0,
+					MinorVersion = 9,
+					RevisionVersion = 99,
+					PatchVersion = 2,
 				};
 			}
 		}
